c68fce85ed74ea10cc68201e44ac1b909e69925d
Ethan Holen <ethanholen@gmail.com>
2020-04-29 10:08:28 -0600

    first commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement Movement;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float ForwardForce = 4000f;
+
+    public float SidewaysForce = 60f;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.AddForce (0 , 0, ForwardForce * Time.deltaTime);
+        //rb.AddForce (SidewaysForce * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey ("a")) {
+            rb.AddForce (-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey ("d")) {
+            rb.AddForce (SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
